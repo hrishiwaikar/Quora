06fe5b73084322d2920617e21b8d17f97682c22e
ultimatehrishi <hrishikeshwaikar@gmail.com>
2019-04-24 22:24:05 -0700

    hi

--- frontend/src/components/questions/question_page.js
+++ frontend/src/components/questions/question_page.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Row, Col } from 'antd';
+import './../../style.css';
+
+import { Typography } from 'antd';
+
+const { Title, Text } = Typography;
 
 
 class QuestionPage extends Component {
@@ -10,7 +17,22 @@ class QuestionPage extends Component {
 
         return (
             <div>
-                This is question Page.
+                <Row className="marginTop-l">
+                    <Col span={13} offset={5}>
+                        <Row>
+                            <Title level={3}>What advice will you give to new programmers?</Title>
+
+                        </Row>
+
+                        <Row className="paddingTop-l">
+                            <Text className="paddingBottom-m" strong>10 Answers</Text>
+                            <hr />
+                        </Row>
+                        <Row>
+
+                        </Row>
+                    </Col>
+                </Row>
             </div>
 
         );
