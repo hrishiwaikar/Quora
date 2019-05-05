388b49e539c71a1534ca97558f8dd8142fb25c48
Vinit Dholakia <vinit.dholakia@gmail.com>
2019-05-04 17:53:39 -0700

    analytics COmmit

--- backend/core/routes/v1route.js
+++ backend/core/routes/v1route.js
@@ -12,82 +12,89 @@ let uploadService = require('../services/uploadService');
 // const upload = require('../services/imageservice');
 
 // const singleUpload = upload.single('profileimage');
+// versionRouter.put("/users/:userId/image", jwt.verifyRequest, function (req, res) {
+        //     singleUpload(req, res, function (err) {
+        //         if (err) {
+        //             return res.status(422).send({
+        //                 errors: [{
+        //                     title: 'Image Upload Error',
+        //                     detail: err.message
+        //                 }]
+        //             });
+        //         }
+    
+        //         return res.json({
+        //             'imageUrl': req.file.location
+        //         });
+        //     });
+        // });
 module.exports = (express) => {
-    let versionRouter = express.Router();
+    try {
+        let versionRouter = express.Router();
 
-    /* Auth Routes */
-    versionRouter.post('/signin', authservice.signin);
-    versionRouter.post('/signup', authservice.signup);
-    /* Auth Routes */
-
-    /* User Routes */
-    //versionRouter.post('/users', userservice.create);
-    versionRouter.get('/users/:userId', jwt.verifyRequest, userservice.read);
-    // versionRouter.put("/users/:userId/image", jwt.verifyRequest, function (req, res) {
-    //     singleUpload(req, res, function (err) {
-    //         if (err) {
-    //             return res.status(422).send({
-    //                 errors: [{
-    //                     title: 'Image Upload Error',
-    //                     detail: err.message
-    //                 }]
-    //             });
-    //         }
-
-    //         return res.json({
-    //             'imageUrl': req.file.location
-    //         });
-    //     });
-    // });
-    versionRouter.put("/users/:userId/image", jwt.verifyRequest, uploadService("profileUpload"), userservice.uploadImage);
-    //versionRouter.get("/users/:userId/image", uploadService('profileRead'));
-    versionRouter.put('/users/:userId', jwt.verifyRequest, userservice.update);
-    versionRouter.delete('/users/:userId', jwt.verifyRequest, userservice.delete);
-    /* User Routes */
-
-    /* Follow Routes */
-    versionRouter.post('/follow/:userId', jwt.verifyRequest, followservice.markFollow); // only self can
-    versionRouter.post('/unfollow/:userId', jwt.verifyRequest, followservice.markUnfollow); // only self can
-    versionRouter.get('/users/:userId/followers', jwt.verifyRequest, followservice.getFollowers);
-    versionRouter.get('/users/:userId/following', jwt.verifyRequest, followservice.getFollowing);
-    /* Follow Routes */
-
-    /* Question Routes */
-
-    versionRouter.get('/feed', jwt.verifyRequest, questionservice.create); // one question one answer - most recent & most upvoted
-
-    versionRouter.post('/questions', jwt.verifyRequest, questionservice.create);
-    versionRouter.get('/questions/:questionId', jwt.verifyRequest, questionservice.read);
-    //versionRouter.put('/questions/:questionId', jwt.verifyRequest, questionservice.update);
-    //versionRouter.delete('/questions/:questionId', jwt.verifyRequest, questionservice.delete);
-    versionRouter.post('/questions/:questionId/follow', jwt.verifyRequest, questionservice.questionFollow); // NOtify Hrishi
-    /* Question Routes */
-
-    /* conversations Routes */
-    versionRouter.get('/conversations/sendto', jwt.verifyRequest, conversationservice.searchUsers);
-    versionRouter.get('/conversations', jwt.verifyRequest, conversationservice.getConversations);
-    versionRouter.get('/conversations/:conversationId', jwt.verifyRequest, conversationservice.getOneConversation); // only self can
-    versionRouter.post('/conversations/message', jwt.verifyRequest, conversationservice.sendMessage);
-    /* conversations Routes */
-
-    /* Answer Routes */
-    versionRouter.post('/answers', jwt.verifyRequest, answerservice.create);
-    versionRouter.post('/answers/:answerId/vote', jwt.verifyRequest, answerservice.upordownvote); // notify Hrishi
-    versionRouter.post('/answers/:answerId/bookmark', jwt.verifyRequest, answerservice.answerBookmark); // notify Hrishi
-    //versionRouter.get('/answers/:questionId', jwt.verifyRequest, answerservice.read);
-    //versionRouter.put('/answers/:questionId', jwt.verifyRequest, answerservice.update);
-    //versionRouter.delete('/answers/:questionId', jwt.verifyRequest, answerservice.delete);
-
-    versionRouter.get('/answers', jwt.verifyRequest, analyticService.getAnswers);
-    versionRouter.get('/answers/:answerId/:type', jwt.verifyRequest, analyticService.getAnswerStats);
-    /* Answer Routes */
-
-    /* Topic Routes */
-    versionRouter.post('/topics', jwt.verifyRequest, topicservice.create);
-    versionRouter.get('/topics', jwt.verifyRequest, topicservice.readMany);
-    versionRouter.put('/topics/:questionId', jwt.verifyRequest, topicservice.update);
-    versionRouter.delete('/topics/:questionId', jwt.verifyRequest, topicservice.delete);
-    /* Topic Routes */
-
-    return versionRouter;
+        /* Auth Routes */
+        versionRouter.post('/signin', authservice.signin);
+        versionRouter.post('/signup', authservice.signup);
+        /* Auth Routes */
+    
+        /* User Routes */
+        //versionRouter.post('/users', userservice.create);
+        versionRouter.get('/users/:userId', jwt.verifyRequest, userservice.read);
+        
+        versionRouter.put("/users/:userId/image", jwt.verifyRequest, uploadService("profileUpload"), userservice.uploadImage);
+        //versionRouter.get("/users/:userId/image", uploadService('profileRead'));
+        versionRouter.put('/users/:userId', jwt.verifyRequest, userservice.update);
+        versionRouter.delete('/users/:userId', jwt.verifyRequest, userservice.delete);
+        /* User Routes */
+    
+        /* Follow Routes */
+        versionRouter.post('/follow/:userId', jwt.verifyRequest, followservice.markFollow); // only self can
+        versionRouter.post('/unfollow/:userId', jwt.verifyRequest, followservice.markUnfollow); // only self can
+        versionRouter.get('/users/:userId/followers', jwt.verifyRequest, followservice.getFollowers);
+        versionRouter.get('/users/:userId/following', jwt.verifyRequest, followservice.getFollowing);
+        /* Follow Routes */
+    
+        /* Question Routes */
+    
+        //versionRouter.get('/feed', jwt.verifyRequest, questionservice.create); // one question one answer - most recent & most upvoted
+    
+        versionRouter.post('/questions', jwt.verifyRequest, questionservice.create);
+        versionRouter.get('/questions/:questionId', jwt.verifyRequest, questionservice.read);
+        //versionRouter.put('/questions/:questionId', jwt.verifyRequest, questionservice.update);
+        //versionRouter.delete('/questions/:questionId', jwt.verifyRequest, questionservice.delete);
+        versionRouter.post('/questions/:questionId/follow', jwt.verifyRequest, questionservice.questionFollow); // NOtify Hrishi
+        /* Question Routes */
+    
+        /* conversations Routes */
+        versionRouter.get('/conversations/sendto', jwt.verifyRequest, conversationservice.searchUsers);
+        versionRouter.get('/conversations', jwt.verifyRequest, conversationservice.getConversations);
+        versionRouter.get('/conversations/:conversationId', jwt.verifyRequest, conversationservice.getOneConversation); // only self can
+        versionRouter.post('/conversations/message', jwt.verifyRequest, conversationservice.sendMessage);
+        /* conversations Routes */
+    
+        /* Answer Routes */
+        versionRouter.post('/answers', jwt.verifyRequest, answerservice.create);
+        versionRouter.get('/answers', jwt.verifyRequest, analyticService.getAnswers);
+        versionRouter.post('/answers/:answerId/vote', jwt.verifyRequest, answerservice.upordownvote); // notify Hrishi
+        versionRouter.post('/answers/:answerId/bookmark', jwt.verifyRequest, answerservice.answerBookmark); // notify Hrishi
+        versionRouter.get('/answers/:answerId/:type', jwt.verifyRequest, analyticService.getAnswerStats);
+        //versionRouter.get('/answers/:questionId', jwt.verifyRequest, answerservice.read);
+        //versionRouter.put('/answers/:questionId', jwt.verifyRequest, answerservice.update);
+        //versionRouter.delete('/answers/:questionId', jwt.verifyRequest, answerservice.delete);
+    
+        versionRouter.get('/views',  analyticService.getViews);
+        /* Answer Routes */
+    
+        /* Topic Routes */
+        versionRouter.post('/topics', jwt.verifyRequest, topicservice.create);
+        versionRouter.get('/topics', jwt.verifyRequest, topicservice.readMany);
+        versionRouter.put('/topics/:questionId', jwt.verifyRequest, topicservice.update);
+        versionRouter.delete('/topics/:questionId', jwt.verifyRequest, topicservice.delete);
+        /* Topic Routes */
+    
+        return versionRouter;
+    } catch (error) {
+        console.error(error)
+    }
+   
 }
--- backend/core/services/analyticservice.js
+++ backend/core/services/analyticservice.js
@@ -6,7 +6,7 @@ module.exports = {
         let limit = parseInt(req.query.top || 10);
         let sort = req.query.sort || "views";
         for (let i = 0; i < limit; i++) {
-            console.log(i,"in")
+            console.log(i, "in")
             ans.push({
                 answerId: (parseInt(Math.random() * 100000)).toString(),
                 questionId: "456",
@@ -38,8 +38,8 @@ module.exports = {
         let obj = {
             answerId: "1234",
             questionId: "456",
-            answer: answers[parseInt(Math.random()*10)],
-            question: questions[parseInt(Math.random()*10)],
+            answer: answers[parseInt(Math.random() * 10)],
+            question: questions[parseInt(Math.random() * 10)],
             graphData: [],
         }
         let startDate = Date.now() - (days * 24 * 60 * 60 * 1000) - 1;
@@ -48,12 +48,8 @@ module.exports = {
                 value: parseInt(Math.random() * 100)
             }
             x.timestamp = (new Date(startDate).getMonth() + 1) + "/" + new Date(startDate).getDate();
-            // if (i % 5 == 0 || i ==1) {
-            // } else {
-            //     x.timestamp = "";
-            // }
             obj.graphData.push(x)
-            startDate = startDate + (24*60*60*1000)
+            startDate = startDate + (24 * 60 * 60 * 1000)
         }
         res.json({
             result: "success",
@@ -63,5 +59,78 @@ module.exports = {
             },
             data: obj
         });
+    },
+    getViews: (req, res, next) => {
+        try {
+            let frequency = req.query.frequency || "day";
+            frequency = frequency.toLowerCase();
+            let type = req.query.type || "login";
+            type = type.toLowerCase();
+            let obj = {
+                frequency: frequency,
+                type: type,
+                graphData: [],
+            }
+            let startDate = null;
+            console.log("CHECK > >> > ");
+            switch (frequency) {
+                case "hour":
+                    startDate = Date.now() - (60 * 60 * 1000);
+                    for (let i = 1; i <= 60; i++) {
+                        obj.graphData.push({
+                            value: parseInt(Math.random() * 120),
+                            timestamp: new Date(startDate).getHours() + ":" + new Date(startDate).getMinutes()
+                        });
+                        startDate = startDate + (1 * 60 * 1000)
+                    }
+                    break;
+                case "day":
+                    startDate = Date.now() - (24 * 60 * 60 * 1000);
+                    for (let i = 1; i <= 24; i++) {
+                        let hours = new Date(startDate).getHours();
+                        let ampm = (hours >= 12 ) ? "pm" : "am"
+                        obj.graphData.push({
+                            value: parseInt(Math.random() * 120),
+                            timestamp: (!!(hours % 12) ? (hours % 12) : 12) + " " + ampm
+                        });
+                        startDate = startDate + (60 * 60 * 1000)
+                    }
+                    break;
+                case "week":
+                    startDate = Date.now() - (7 * 24 * 60 * 60 * 1000) - 1;
+                    for (let i = 1; i <= 7; i++) {
+                        obj.graphData.push({
+                            value: parseInt(Math.random() * 150),
+                            timestamp: (new Date(startDate).getMonth()+1) + "/" + new Date(startDate).getDate()
+                        });
+                        startDate = startDate + (24 * 60 * 60 * 1000)
+                    }
+                    break;
+                case "month":
+                    startDate = Date.now() - (30 * 24 * 60 * 60 * 1000) - 2;
+                    for (let i = 1; i <= 31; i++) {
+                        obj.graphData.push({
+                            value: parseInt(Math.random() * 150),
+                            timestamp:( new Date(startDate).getMonth()+1) + "/" + new Date(startDate).getDate()
+                        });
+                        startDate = startDate + (24 * 60 * 60 * 1000)
+                    }
+                    break;
+                default:
+                    break;
+            }
+            console.log("CHECK  < < < < < > >> > ");
+            res.json({
+                result: "success",
+                response: {
+                    message: "Data fetched Successfully",
+                    code: "DATA"
+                },
+                data: obj
+            });
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 }
