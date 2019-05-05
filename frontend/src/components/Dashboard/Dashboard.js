a8d3cf1da7baab2b967062a61fa6aee1b85ea585
Atul Gutal <atulgutal@Atuls-MacBook-Pro.local>
2019-05-04 20:19:36 -0700

    dashboard implemented

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,37 +1,122 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
 import "./DashboardCharts.css";
-//import axios from "axios";
+import axios from "axios";
 
 import DashboardCharts from "./DashboardCharts";
 // import { Element } from "react-faux-dom";
 // import * as d3 from "d3";
-import { Button, Row, Col, Menu, Dropdown, AutoComplete } from "antd";
+import { Row, Col, Select } from "antd";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      graphData: {},
+      xAxis: {}
+    };
+  }
+  makeGraph = (graphRange, graphType) => {
+    console.log(graphRange, graphType);
+    const token =
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhMTY0MDIyMC02ODAyLTExZTktOGUxYS1iZDE3NmJmNjdkYWEiLCJjcmVhdGVkX2F0IjoxNTU2NzQzMTcwMzIyLCJpYXQiOjE1NTY3NDMxNzAsImV4cCI6MTU1OTMzNTE3MH0.CNknpZADc05g6Un4ogHzO8pmdW5mDB3QnhRLQKHcmm0";
+
+    axios
+      .get("/v1/views?frequency=" + graphRange + "&type=" + graphType, {
+        headers: {
+          authorization: token
+        }
+      })
+      .then(res => {
+        if (res.status === 200) {
+          // console.log("view response data", res.data.data.graphData);
+
+          let result = res.data.data.graphData.map((value, i) => {
+            return value["value"];
+          });
+
+          let result1 = res.data.data.graphData.map((date, i) => {
+            return date["timestamp"];
+          });
+
+          //   console.log("result", result1);
+          let graphData = this.state.graphData || {};
+          let xAxis = this.state.xAxis || {};
+
+          graphData[graphType] = result;
+          xAxis[graphType] = result1;
+
+          this.setState({
+            graphData: graphData,
+            xAxis: xAxis
+          });
+        }
+      })
+      .catch(err => {
+        console.log("view error: ", err);
+      });
+  };
+  makeAllGraph = value => {
+    let graphRange = value;
+    let graphAttr = [
+      { graphRange: graphRange, graphType: "login" },
+      { graphRange: graphRange, graphType: "signup" },
+      { graphRange: graphRange, graphType: "questions" },
+      { graphRange: graphRange, graphType: "answers" },
+      { graphRange: graphRange, graphType: "comments" }
+    ];
+
+    graphAttr.map((d, i) => {
+      this.makeGraph(d.graphRange, d.graphType);
+    });
+  };
+  componentDidMount() {
+    this.makeAllGraph("hour");
+  }
+
   render() {
     let chartData = [
       {
         name: "Login",
-        color: "#ff6361"
+        color: "#ff6361",
+        graphData: this.state.graphData["login"],
+        xAxis: this.state.xAxis["login"]
       },
       {
         name: "Signup",
-        color: "#bc5090"
+        color: "#bc5090",
+        graphData: this.state.graphData["signup"],
+        xAxis: this.state.xAxis["signup"]
       },
       {
         name: "Questions",
-        color: "#58508d"
+        color: "#58508d",
+        graphData: this.state.graphData["questions"],
+        xAxis: this.state.xAxis["questions"]
       },
       {
         name: "Answer",
-        color: "#003f5c"
+        color: "#003f5c",
+        graphData: this.state.graphData["answers"],
+        xAxis: this.state.xAxis["answers"]
       },
       {
         name: "Comments",
-        color: "#ffa600"
+        color: "#ffa600",
+        graphData: this.state.graphData["comments"],
+        xAxis: this.state.xAxis["comments"]
       }
     ];
+
+    const Option = Select.Option;
+
+    function handleChange(value) {
+      console.log(`selected ${value}`);
+    }
+
+    console.log(chartData);
+
     return (
       <Row
         style={{
@@ -39,12 +124,40 @@ class Dashboard extends Component {
           marginLeft: "40px"
         }}
       >
-        <h4 className="dashboardChartTitle">Quora Dashboard</h4>
-        {chartData.map((value, i) => (
-          <Col span={8}>
-            <DashboardCharts name={value.name} color={value.color} />
+        <Row>
+          <Col span={24}>
+            <Col span={8}>
+              <h4 className="dashboardChartTitle">Quora Dashboard</h4>
+            </Col>
+            <Col span={8} />
+            <Col span={8} style={{ float: "right", marginRight: "80px" }}>
+              <Select
+                defaultValue="hour"
+                style={{ width: 120 }}
+                onChange={this.makeAllGraph}
+                style={{ float: "right" }}
+              >
+                <Option value="hour">Hour</Option>
+                <Option value="day">Day</Option>
+                <Option value="week">Week</Option>
+                <Option value="month">Month</Option>
+                <Option value="year">Year</Option>
+              </Select>
+            </Col>
           </Col>
-        ))}
+        </Row>
+        <Row>
+          {chartData.map((value, i) => (
+            <Col span={8}>
+              <DashboardCharts
+                name={value.name}
+                color={value.color}
+                graphData={value.graphData}
+                xAxis={value.xAxis}
+              />
+            </Col>
+          ))}
+        </Row>
       </Row>
     );
   }
--- frontend/src/components/Dashboard/DashboardCharts.js
+++ frontend/src/components/Dashboard/DashboardCharts.js
@@ -2,76 +2,9 @@ import React, { Component } from "react";
 import "antd/dist/antd.css";
 import "./../../style.css";
 import Highcharts from "highcharts";
-
-//import $ from "jquery";
 import HighchartsReact from "highcharts-react-official";
-import axios from "axios";
-// import { Element } from "react-faux-dom";
-// import * as d3 from "d3";
-// import { Button, Row, Col, Menu, Dropdown } from "antd";
 
 class DashboardCharts extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      graphData: "",
-      xAxis: []
-    };
-  }
-
-  componentDidMount() {
-    const token =
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhMTY0MDIyMC02ODAyLTExZTktOGUxYS1iZDE3NmJmNjdkYWEiLCJjcmVhdGVkX2F0IjoxNTU2NzQzMTcwMzIyLCJpYXQiOjE1NTY3NDMxNzAsImV4cCI6MTU1OTMzNTE3MH0.CNknpZADc05g6Un4ogHzO8pmdW5mDB3QnhRLQKHcmm0";
-
-    axios
-      .get("v1/answers?top=10&sort=views", {
-        headers: {
-          authorization: token
-        }
-      })
-      .then(res => {
-        if (res.status === 200) {
-          console.log("view response data", res.data.answers);
-
-          this.setState({
-            answerId: res.data.answers[0].answerId
-          });
-
-          axios
-            .get("v1/answers/" + this.state.answerId + "/views?day=30", {
-              headers: {
-                authorization: token
-              }
-            })
-            .then(res => {
-              if (res.status === 200) {
-                console.log("graph response data", res.data.data.graphData);
-                let result = res.data.data.graphData.map((value, i) => {
-                  return value["value"];
-                });
-
-                let result1 = res.data.data.graphData.map((date, i) => {
-                  return date["timestamp"];
-                });
-
-                console.log("result", result1);
-
-                this.setState({
-                  graphData: result,
-                  xAxis: result1
-                });
-              }
-            })
-            .catch(err => {
-              console.log("view error: ", err);
-            });
-        }
-      })
-      .catch(err => {
-        console.log("view error: ", err);
-      });
-  }
   render() {
     const options = {
       chart: {
@@ -84,7 +17,7 @@ class DashboardCharts extends Component {
       },
       xAxis: {
         //visible: true,
-        categories: this.state.xAxis
+        categories: this.props.xAxis
       },
       yAxis: {
         title: {
@@ -95,7 +28,7 @@ class DashboardCharts extends Component {
         {
           name: this.props.name,
           type: "area",
-          data: this.state.graphData,
+          data: this.props.graphData,
           color: this.props.color
         }
       ],
