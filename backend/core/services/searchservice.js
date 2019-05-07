f272f5a0c7827fbf1dc6cea1b4c8e0d7d4a26311
tanajijadhav <tanajigjadhav@gmail.com>
2019-05-06 18:47:54 -0700

    search imconplete

--- backend/core/services/searchservice.js
+++ backend/core/services/searchservice.js
@@ -1,17 +1,31 @@
 const rs = require("./../commons/responses");
 const utils = require("./../commons/utils");
+let questionModel = require("../models/questionmodel")
+let topicModel = require("../models/topicmodel")
+let userModel = require("../models/usermodel")
 
 let service = {
     search: (...args) => {
         return new Promise(function (resolve, reject) {
             try {
-                let _session = args[0] || {};
-                let body = args[1] || {};
+                
+                // let search
 
+                // let output = []
+                // let _session = args[0] || {};
+                // let body = args[1] || {};
+                // let searchTerm = body.searchTerm || null;
+                // if (searchTerm === null){
+                //     console.error("Nothing in the search term, so sending empty list in response")
+                //     return resolve(output)
+                // }
+                // else{
+                //     questionModel.find({questionText:new RegExp(filter_value, "i")}).select({questionId:1,questionText:1})..limit(page_limit)
+                // }
                 resolve([])
             } catch (e) {
                 console.error(e)
-                reject(e);
+                return reject(e);
             }
         });
     }
